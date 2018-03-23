8d2b1573802bd9e7e141cc613a004f332d701c6a
fangcun <fangcun@stepgame.net>
2018-03-23 20:44:08 +0800

    Edit

--- Engine.h
+++ Engine.h
@@ -3,9 +3,14 @@
 
 #include "Vector.h"
 
+typedef void (*Func) ();                                 //函数回调
+
 typedef struct tagSprite
 {
-
+    int x,y;
+    Func DoCal;                                            //执行计算
+    Func DoDraw;                                           //执行绘制
+    Func DoEvents;                                         //执行事件
 } Sprite;
 
 typedef struct tagScene
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <GL/gl.h>
-#include "SGEngine.h"
+#include "Engine.h"
 
 void Display()
 {
